e49a0b79d37ec90bc204f014ea8b21f8a7dd39e2
aguldstrand <golfin85@gmail.com>
2015-09-06 20:53:15 +0200

    webpack

---
+++ src/Tasky/gulpfile.js
@@ -0,0 +1,23 @@
+/// <binding AfterBuild='default' />
+/*
+This file in the main entry point for defining Gulp tasks and using Gulp plugins.
+Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
+*/
+
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var webpack = require('webpack');
+var webpackConfig = require("./webpack.config.js");
+
+gulp.task('default', ["webpack"]);
+
+gulp.task('webpack', function (callback) {
+
+    webpack(webpackConfig, function (err, stats) {
+        if (err) throw new gutil.PluginError("webpack", err);
+        gutil.log("[webpack]", stats.toString({
+            colors: true
+        }));
+        callback();
+    });
+});
---
+++ src/Tasky/webpack.config.js
@@ -0,0 +1,26 @@
+﻿module.exports = {
+    entry: "./client/main.js",
+    output: {
+        path: "./wwwroot/assets",
+        filename: "bundle.js"
+    },
+    module: {
+        loaders: [{
+            test: /\.css$/,
+            exclude: /\.useable\.css$/,
+            loader: "style!css"
+        }, {
+            test: /\.useable\.css$/,
+            loader: "style/useable!css"
+        }, {
+            test: /\.css$/,
+            loaders: 'style!css'
+        }, {
+            test: /\.css$/,
+            loaders: 'style!css'
+        }, {
+            test: /\.scss$/,
+            loader: 'style!css!sass'
+        }]
+    }
+};

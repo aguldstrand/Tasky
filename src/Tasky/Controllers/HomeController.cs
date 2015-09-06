ae492dcae786a156fd9ef973c1f9ebf5caf001c7
aguldstrand <golfin85@gmail.com>
2015-09-06 20:05:07 +0200

    a ui in progress

---
+++ src/Tasky/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNet.Mvc;
+
+namespace Tasky.Controllers
+{
+    [Route("/")]
+    public class HomeController : Controller
+    {
+        /*
+        private readonly ProjectsController projects;
+        private readonly SprintsController sprints;
+        private readonly IssuesController issues;
+        private readonly CommentsController comments;
+        private readonly AttachmentsController attachments;
+
+        public HomeController(ProjectsController projects,SprintsController sprints,IssuesController issues,CommentsController comments, AttachmentsController attachments)
+        {
+            this.projects = projects;
+            this.sprints = sprints;
+            this.issues = issues;
+            this.comments = comments;
+            this.attachments = attachments;
+        }
+        */
+
+        [HttpGet]
+        public IActionResult Index(int? project, int? sprint)
+        {
+            return View();
+        }
+    }
+}
---
+++ src/Tasky/ViewModels/Home/Index.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Threading.Tasks;
+using Tasky.Models;
+using Tasky.Services;
+
+namespace Tasky.ViewModels.Home
+{
+    public class Index
+    {
+        public ImmutableArray<IdentityWrapper<Project>> Projects { get; }
+        public ImmutableArray<IdentityWrapper<Project, Sprint>> Sprints { get; }
+        public ImmutableArray<IdentityWrapper<Project, Sprint, Issue>> Issues { get; }
+    }
+}
